48aaf8ea86aa7b01ab4ba06d54d0189a562ede7b
Cass <cassy.pernelle@gmail.com>
2025-01-10 11:08:12 +0100

    navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,11 +1,11 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import { signIn, signOut, getProviders, useSession } from "next-auth/react";
+import { signOut, getProviders, useSession } from "next-auth/react";
 import Image from "next/image";
 
 export function NavBar() {
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
   const [providers, setProviders] = useState<any>(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
@@ -17,18 +17,11 @@ export function NavBar() {
     setUpProviders();
   }, []);
 
-
-  const generalLinkstyle = "p-4 hover:bg-[#4F4141]";
-  const selectedLinkstyle =
-    "p-4 hover:bg-[#4F4141] bg-gray-300 text-gray-700 hover:text-gray-500 font-semibold border-b-4 border-[#E7A100]";
-
   return (
     <div className="font-roboto">
-
       {/* Desktop Navigation */}
       <nav className="bg-red-500 p-4 sticky top-0 z-10 md:flex hidden">
         <div className="flex flex-wrap justify-between w-screen items-center mx-auto max-w-screen-xl">
-          
           <a href="/" className="text-white flex items-center">
             <Image
               className="w-6 h-6 mt-1 mr-2"
@@ -39,21 +32,21 @@ export function NavBar() {
             />
             <p className="self-center text-2xl font-semibold">Liste en Poche</p>
           </a>
-          
+
           <div className="flex items-center gap-4">
             {session?.user ? (
               <>
-                <Link href={`/${session?.user.id}/create`}>
+                <Link href={`/${session.user.id}/create`}>
                   <button className="text-white text-lg px-4 py-2 rounded">
                     Créer une nouvelle liste
                   </button>
                 </Link>
-                <Link href={`/${session?.user.id}/listes`}>
+                <Link href={`/${session.user.id}/listes`}>
                   <button className="text-white text-lg px-4 py-2 rounded">
                     Toutes mes listes
                   </button>
                 </Link>
-                <Link href={`/${session?.user.id}`}>
+                <Link href={`/${session.user.id}`}>
                   <button className="text-white text-lg px-4 py-2 rounded">
                     Mon Profil
                   </button>
@@ -75,7 +68,6 @@ export function NavBar() {
         </div>
       </nav>
 
-
       {/* Mobile Navigation */}
       <div className="md:hidden flex fixed z-40 w-screen">
         <div className="flex justify-between w-full bg-red-500 p-4">
@@ -91,7 +83,7 @@ export function NavBar() {
           </a>
           <div
             onClick={() => setToggleDropdown((prev) => !prev)}
-            className="mr-4 my-auto w-6 h-6 z-50"
+            className="mr-4 my-auto w-6 h-6 z-50 cursor-pointer"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -113,34 +105,52 @@ export function NavBar() {
         {/* Dropdown Menu */}
         {toggleDropdown && (
           <div className="absolute right-0 top-full rounded-bl-lg w-[50%] p-5 bg-red-500 min-w-[210px] flex flex-col gap-2 justify-end items-end text-white">
-            <Link href="/" className="my-4 text-lg" onClick={() => setToggleDropdown(false)}>
+            <Link
+              href="/"
+              className="my-4 text-lg"
+              onClick={() => setToggleDropdown(false)}
+            >
               Accueil
             </Link>
-            <Link
-              href="/new-list"
-              className="my-4 text-lg"
-              onClick={() => setToggleDropdown(false)}
-            >
-              Nouvelle liste
-            </Link>
-            
-            <Link
-              href="/profil"
-              className="my-4 text-lg"
-              onClick={() => setToggleDropdown(false)}
-            >
-              Mon profil
-            </Link>
             {session?.user ? (
-              <button
-                onClick={() => signOut()}
-                className="bg-white text-red-400 border-2 border-white px-6 py-2 rounded-full hover:bg-red-600 hover:text-white font-bold"
-              >
-                Déconnexion
-              </button>
+              <>
+                <Link
+                  href={`/${session.user.id}/create`}
+                  className="my-4 text-lg"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Nouvelle liste
+                </Link>
+                <Link
+                  href={`/${session.user.id}/listes`}
+                  className="my-4 text-lg"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Mes listes
+                </Link>
+                <Link
+                  href={`/${session.user.id}`}
+                  className="my-4 text-lg"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Mon Profil
+                </Link>
+                <button
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className="bg-white text-red-400 border-2 border-white px-6 py-2 rounded-full hover:bg-red-600 hover:text-white font-bold"
+                >
+                  Déconnexion
+                </button>
+              </>
             ) : (
               <Link href="/login">
-                <button className="bg-white text-red-400 border-2 border-white px-6 py-2 rounded-full hover:bg-red-600 hover:text-white font-bold">
+                <button
+                  onClick={() => setToggleDropdown(false)}
+                  className="bg-white text-red-400 border-2 border-white px-6 py-2 rounded-full hover:bg-red-600 hover:text-white font-bold"
+                >
                   Connexion
                 </button>
               </Link>
