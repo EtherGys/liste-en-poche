aa43e097e315e70f2a48c30529f1a8863920b6e2
thomasdindin <thomas.dindin@gmail.com>
2025-01-10 11:12:26 +0100

    Plus d'erreur sur l'id utilisateur

--- app/api/user/[id]/route.ts
+++ app/api/user/[id]/route.ts
@@ -1,18 +1,19 @@
 "use server"
 import { NextResponse, NextRequest } from "next/server";
 import prisma from "@/utils/prisma";
+import {validateSession} from "@/utils/validateSession";
 
 
 // Update user 
 export async function PUT(req: NextRequest, params: any) {
     
     const { mail, nom, prenom } = await req.json();
-    const id = await params.params.id
+    const userId = parseInt(await validateSession(req));
     try {
         
         const user = await prisma.utilisateurs.update({
             
-            where: { id_utilisateur: Number(id) },
+            where: { id_utilisateur: Number(userId) },
             
             
             data: { mail, nom, prenom },
@@ -31,11 +32,11 @@ export async function PUT(req: NextRequest, params: any) {
 
 // Get user 
 export async function GET(req: NextRequest, params: any) {
-    const id = await params.params.id
+    const userId = parseInt(await validateSession(req));
     try {
         const user = await prisma.utilisateurs.findUnique({
             
-            where: { id_utilisateur: Number(id) },
+            where: { id_utilisateur: Number(userId) },
             
             
         });
@@ -46,6 +47,6 @@ export async function GET(req: NextRequest, params: any) {
         
     } catch (error) {
         console.log(error);
-        return new Response(`Failed to get user ${id}`, { status: 500 })
+        return new Response(`Failed to get user ${userId}`, { status: 500 })
     }
 }
